aadfe29ed8f39baba50fffb95fd8e0e5b56fc7b3
ashish-rio <riooencoder@gmail.com>
2024-10-08 09:06:30 +0530

    Create recursion.js

---
+++ recursion.js
@@ -0,0 +1,25 @@
+use recursion to find the factorial of a number in javascript.
+
+// recursive function
+function factorial(num) {
+
+    // base case
+    // recurse only if num is greater than 0
+    if (num > 1) {
+        return num * factorial(num - 1);
+    }
+    else {
+        return 1;
+    };
+};
+
+let x = 3;
+
+// store result of factorial() in variable
+let y = factorial(x);
+
+console.log(`The factorial of ${x} is ${y}`);
+
+Output
+
+The factorial of 3 is 6.
